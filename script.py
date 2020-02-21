92f6b7343f1caf9d3cc1d3666c5de3a4f1b67645
clarkmyfancy <clark.jack0@gmail.com>
2020-02-21 01:26:29 -0600

    now runs with pygame and displays randopm list...badly

--- script.py
+++ script.py
@@ -1,17 +1,44 @@
+from Gui import Gui
+from ListGenerator import Generator
 from Sorter import Sorter
 
+import pygame
+
+pygame.init()
+
+HEIGHT = 600
+WIDTH = 600
+
+
 def main():
+
+    surface = (WIDTH, HEIGHT)
+    window = pygame.display.set_mode(surface)
+    pygame.display.set_caption("Sudoku Solver")
+    gui = Gui(HEIGHT, WIDTH)
+
+    g = Generator(25, 25)
+    random_list = g.generate_random_list()
     s = Sorter()
 
-    print("Recursive List", end = "")
-    recursive_selection_sort = s.sort("rs")
-    print(recursive_selection_sort)
-    print()
+    running = True
+    while running: 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        gui.render_list(window, random_list)
 
-    print("Iterative List", end = "")
-    iterative_selection_sort = s.sort("is")
-    print(iterative_selection_sort)
-    print()
+        # print("Recursive List", end = "")
+        # recursive_selection_sort = s.sort("rs")
+        # print(recursive_selection_sort)
+        # print()
+
+        # print("Iterative List", end = "")
+        # iterative_selection_sort = s.sort("is")
+        # print(iterative_selection_sort)
+        # print()
+        pygame.display.update()
     
 if __name__ == "__main__":
     main()
+    pygame.quit()
