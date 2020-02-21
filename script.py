e7038a187c79a64370fb1e30838f9270176b94ea
Conrad <9323501+clarkmyfancy@users.noreply.github.com>
2020-02-20 18:49:33 -0600

    adding recursive version of selection sort

--- script.py
+++ script.py
@@ -3,9 +3,15 @@ from Sorter import Sorter
 _list = [7,2,6,12,7,1,3,7]
 
 def main():
-    sorter = Sorter(_list)
-    sortedList = sorter.runSelectionSortIter()
+    s = Sorter(_list)
+    # this is not good because the sort algorithm sorts 'in-place' which changes the 
+    # list. the class should have a static list member that is used. when a new sort 
+    # algorigm is used the sort function should copy the static list over to a mutatable list
+    # and use and eventually return that to the console
+    sortedList = s.runSelectionSortIteratively()
     print(sortedList)
+    diffMethodSortedList = s.runSelectionSortRecursively()
+    print(diffMethodSortedList)
     
 
 if __name__ == "__main__":
