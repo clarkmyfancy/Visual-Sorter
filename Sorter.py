db0ed90c17e9ad86debddcd39c02260ce6684374
clarkmyfancy <clark.jack0@gmail.com>
2020-02-21 00:07:13 -0600

    recursive, iterative selection sort

--- Sorter.py
+++ Sorter.py
@@ -1,27 +1,54 @@
+from ListGenerator import Generator
+
 class Sorter:
+    def __init__(self):
+        self.list_factory = Generator(20, 10)
 
-    def __init__(self, _list):
-        self._list = _list
+    def sort(self, choice):
+        if choice == 'rs':
+            return self.recursive_selection_sort()
+        elif choice == 'is':
+            return self.iterative_selection_sort()
 
-    def runSelectionSortIteratively(self):
+    def recursive_selection_sort(self):
+        _list = self.list_factory.generate_random_list()
+        print()
+        print(_list)
         startIndexOfUnsortedList = 0
-        for x in range(startIndexOfUnsortedList, len(self._list)):
-            smallestVal = self._list[x]
+        for x in range(startIndexOfUnsortedList, len(_list)):
+            smallestVal = _list[x]
             smallestValIndex = -1
             smallestValWasChanged = False
-            for y in range(startIndexOfUnsortedList + 1, len(self._list)):
-                if self._list[y] < smallestVal:
-                    smallestVal = self._list[y]
+            for y in range(startIndexOfUnsortedList + 1, len(_list)):
+                if _list[y] < smallestVal:
+                    smallestVal = _list[y]
                     smallestValWasChanged = True
                     smallestValIndex = y
             if smallestValWasChanged:
-                tempValue = self._list[x]
-                self._list[x] = smallestVal
-                self._list[smallestValIndex] = tempValue
+                tempValue = _list[x]
+                _list[x] = smallestVal
+                _list[smallestValIndex] = tempValue
             startIndexOfUnsortedList += 1
-        return self._list
+        return _list
 
-    def runSelectionSortRecursively(self):
-        smallestVal = self._list[0]
+    def iterative_selection_sort(self):
+        _list = self.list_factory.generate_random_list()
+        print()
+        print(_list)
+        self.s_sort(_list)
+        return _list
 
-        return self._list
+    def s_sort(self, _list, first = 0):
+        length = len(_list)
+        if first == length:
+            return -1
+        candidate_smallest = self.findMinIndex(_list, first, length - 1)
+        if candidate_smallest != first:
+            _list[candidate_smallest], _list[first] = _list[first], _list[candidate_smallest]
+        return self.s_sort(_list, first + 1)
+
+    def findMinIndex(self, _list, first, last):
+        if first == last:
+            return first
+        smallest_index = self.findMinIndex(_list, first + 1, last)
+        return first if _list[first] < _list[smallest_index] else smallest_index
