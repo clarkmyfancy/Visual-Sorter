0f269697292dea5798234894344fd92e0e1839f2
clarkmyfancy <clark.jack0@gmail.com>
2020-02-21 00:07:59 -0600

    refactored sort caller and cleaned code

--- script.py
+++ script.py
@@ -1,18 +1,17 @@
 from Sorter import Sorter
 
-_list = [7,2,6,12,7,1,3,7]
-
 def main():
-    s = Sorter(_list)
-    # this is not good because the sort algorithm sorts 'in-place' which changes the 
-    # list. the class should have a static list member that is used. when a new sort 
-    # algorigm is used the sort function should copy the static list over to a mutatable list
-    # and use and eventually return that to the console
-    sortedList = s.runSelectionSortIteratively()
-    print(sortedList)
-    diffMethodSortedList = s.runSelectionSortRecursively()
-    print(diffMethodSortedList)
-    
+    s = Sorter()
 
+    print("Recursive List", end = "")
+    recursive_selection_sort = s.sort("rs")
+    print(recursive_selection_sort)
+    print()
+
+    print("Iterative List", end = "")
+    iterative_selection_sort = s.sort("is")
+    print(iterative_selection_sort)
+    print()
+    
 if __name__ == "__main__":
     main()
